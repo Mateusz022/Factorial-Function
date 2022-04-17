8a06bb654c08a94158b6121ed7a895c2175da153
Mateusz <100945821+Mateusz022@users.noreply.github.com>
2022-04-17 18:23:26 +0200

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace silnia_rekurencyjnieeeee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 4;
+            Console.WriteLine("Silnia dla " + liczba + " to: ");
+            Console.WriteLine(wyliczSilnie(liczba));
+            Console.ReadKey();
+        }
+        static int wyliczSilnie(int n)
+            {
+                if (n <= 1) return 1;
+                else return n * wyliczSilnie(n - 1);
+            }
+
+        }
+    }
+            // Bez rekurencji
+        static int Silnia(int n)
+        {
+    int s = 1;
+    for (int i = 1; i <= n; i++)
+        s = s * i;
+    return (s);
+        }
+        // Z rekurencją
+        static int Silnia(int n)
+        {
+    if (n <= 1)
+        return 1;
+    else
+        return n * Silnia(n - 1);
+        }
+
